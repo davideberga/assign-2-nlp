eba9725c5ff30fe3d7afb65b9917e3856043ca83
davide <tu@esempio.co>
2023-02-25 23:07:55 +0100

    change to github repository

---
+++ README.md
@@ -0,0 +1,107 @@
+# Assignment 2 NLP
+
+## Introduction
+
+This project contains a pipeline to produce the syntax tree of a sentence, given its language. The approach used is the pure symbolic one with a context free grammar as a base.
+
+## Structure
+
+- ```grammars/``` : here there are the grammars (*.cfg) used, one for each language
+- ```POSTagger/``` : the PArt-of-Speech tagger used, downloaded from this [repository](https://github.com/datquocnguyen/RDRPOSTagger). It is the RDRPOSTagger, a rule base tagger with models for about 330 languages. This folder contains also the most recent models.
+- ```main.py``` : the script to run the pipeline
+
+## Usage
+
+Inside the ```main.py``` edit the language and the sentence:
+
+```
+lang = ["en" | "it" | "fr" | "ge"] : string
+sentence = "A simple sentence" : string
+```
+
+then ```python3 main.py```.
+
+## Examples
+
+Input (en): 
+```
+lang = "en"
+sentence = "The fox is on the table and a small turtle is on the floor of the building ."
+```
+
+Output: 
+```
+>>>>> First admissible tree: 
+(S
+  (S
+    (S
+      (NP (DET The) (NOUN fox))
+      (VP (VERB is) (PP (ADP on) (NP (DET the))) (NP (NOUN table))))
+    (CCONJ and)
+    (S
+      (NP (DET a) (ADJS (ADJ small) (ADJS (ADJ turtle))))
+      (VP
+        (VERB is)
+        (PP
+          (ADP on)
+          (NP (DET the) (NOUN floor) (PP (ADP of) (NP (DET the)))))
+        (NP (NOUN building)))))
+  (PUNCT .))
+```
+
+Input (it): 
+```
+lang = "it"
+sentence = "Il cane è salito improvvisamente su una mensola"
+```
+
+Output: 
+```
+>>>>> First admissible tree: 
+(S
+  (S (NP (DET Il) (NOUN cane)) (VP (VERB (AUX è))))
+  (S
+    (NP )
+    (VP
+      (VERB salito)
+      (ADVS (ADV improvvisamente))
+      (PP (ADP su) (NP (DET una)))
+      (NP (NOUN mensola)))))
+```
+
+Input (fr): 
+```
+lang = "fr"
+sentence = "Le chien a soudainement grimpé sur une étagère"
+```
+
+Output: 
+```
+>>>>> First admissible tree: 
+(S
+  (S
+    (NP (DET Le) (NOUN chien))
+    (VP (VERB (AUX a)) (ADVS (ADV soudainement))))
+  (S
+    (NP )
+    (VP
+      (VERB grimpé)
+      (PP (ADP sur) (NP (DET une)))
+      (NP (NOUN étagère)))))
+```
+
+Input (ge): 
+```
+lang = "ge"
+sentence = "Die böse Hexe durchstreift den Wald"
+```
+
+Output: 
+```
+>>>>> First admissible tree: 
+(S
+  (NP (DET Die) (ADJS (ADJ böse)) (NOUN Hexe))
+  (VP (VERB durchstreift) (NP (DET den) (NOUN Wald))))
+```
+
+
---
+++ main.py
@@ -0,0 +1,103 @@
+from typing import List
+from POSTagger.Utility.Utils import readDictionary
+from POSTagger.RDRPOSTagger.RDRPOSTagger import POSTagger
+import nltk
+from nltk import CFG
+from nltk import EarleyChartParser
+from nltk import word_tokenize
+from nltk import Tree
+
+import re
+
+nltk.download('punkt')
+
+POS_TAG_MODELS = {
+    "en" : { 
+      "dict" : "./POSTagger/Models/ud-treebanks-v2.4/UD_English-LinES/en_lines-ud-train.conllu.UPOS.DICT",
+      "rdr" : "./POSTagger/Models/ud-treebanks-v2.4/UD_English-LinES/en_lines-ud-train.conllu.UPOS.RDR"
+    },
+    "it" : {
+      "dict" : "./POSTagger/Models/ud-treebanks-v2.4/UD_Italian-ISDT/it_isdt-ud-train.conllu.UPOS.DICT",
+      "rdr" : "./POSTagger/Models/ud-treebanks-v2.4/UD_Italian-ISDT/it_isdt-ud-train.conllu.UPOS.RDR" 
+    },
+    "fr" : {
+      "dict" : "./POSTagger/Models/ud-treebanks-v2.4/UD_French-ParTUT/fr_partut-ud-train.conllu.UPOS.DICT",
+      "rdr" : "./POSTagger/Models/ud-treebanks-v2.4/UD_French-ParTUT/fr_partut-ud-train.conllu.UPOS.RDR"
+    },
+    "ge" : {
+      "dict" : "./POSTagger/Models/ud-treebanks-v2.4/UD_German-GSD/de_gsd-ud-train.conllu.UPOS.DICT",
+      "rdr" : "./POSTagger/Models/ud-treebanks-v2.4/UD_German-GSD/de_gsd-ud-train.conllu.UPOS.RDR"
+    }
+}
+
+LANGUAGE_EXTENDED = {
+    "it" : "italian",
+    "en" : "english",
+    "ge" : "german",
+    "fr" : "french"
+}
+
+def extract_pos_tags(text: str):
+    rgr = re.compile(r'(\w+)/(\w+)')
+    res = []
+    for m in rgr.finditer(text):
+        res.append((m.group(1), m.group(2)))
+    return res
+
+def pos_tag(text : str, lang : str):
+    language = POS_TAG_MODELS[lang]
+    
+    pos_tagger = POSTagger()
+    pos_tagger.constructSCRDRtreeFromRDRfile(language["rdr"]) 
+    DICT = readDictionary(language["dict"])
+    text_tagged = pos_tagger.tagRawSentence(DICT, text)
+    
+    return extract_pos_tags(text_tagged)
+    
+def load_grammar(terminals: List, lang: str):
+    grammar = ""
+    path = './grammars/' + lang + '.cfg'
+    with open(path) as cfg:
+        grammar = cfg.readlines()
+
+        for (token, upos) in terminals:
+            grammar.append("{lhs} -> '{rhs}'".format(lhs=upos, rhs=token))
+            
+    return CFG.fromstring(grammar)
+    
+def remove_empty_nodes(tree):
+    if isinstance(tree, nltk.tree.Tree) and len(tree) == 0:
+        return None
+    if isinstance(tree, nltk.tree.Tree):
+        children = [remove_empty_nodes(child) for child in tree]
+        children = [child for child in children if child is not None]
+        return Tree(tree.label(), children)
+    return tree
+
+# EDIT to test
+lang = "ge"
+sentence = "Die böse Hexe durchstreift den Wald"
+
+langExtended = LANGUAGE_EXTENDED[lang]
+
+pos_tags = pos_tag(sentence, lang)
+
+print("\n>>>>> POS TAGAGGING: ")
+print(">>>>> " + str(pos_tags))
+
+grammar = load_grammar(pos_tags, lang)
+
+parser = EarleyChartParser(grammar, trace=0)
+
+text_tokenized = word_tokenize(sentence, langExtended)
+
+print("\n>>>>> First admissible tree: ")
+for tree in parser.parse(text_tokenized):
+    print(remove_empty_nodes(tree))
+    # Print only the first one, ambiguity blocked here
+    break
+
+
+
+
+
